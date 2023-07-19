522872bb9d58e3042f843d38d3c299840f87f18a
Kanat Bayalbayev <Kanatkz07@mail.ru>
2023-07-19 15:02:46 +0600

    created Domain

--- app/src/main/java/com/example/countgame/presentation/MainActivity.kt
+++ app/src/main/java/com/example/countgame/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.countgame
+package com.example.countgame.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.countgame.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
